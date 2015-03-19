54d338474204e14771f165917b810fc281bf9f3b
Kenshi Takayama <kenshi84@gmail.com>
2015-03-19 15:23:06 +0900

    bit simplified

--- common.js
+++ common.js
@@ -27,110 +27,71 @@ function get_shader(gl, vertex_shader_src, fragment_shader_src) {
 
 function get_legacygl(gl, shader_program) {
     var legacygl = {};
-    // legacygl.current_color = [1, 1, 1, 1];
-    // legacygl.current_normal = [0, 0, 1];
-    // legacygl.current_texcoord = [0, 0];
     legacygl.is_valid = false;
-    // legacygl.a_position = gl.getAttribLocation(shader_program, "a_position");
-    // legacygl.a_color = gl.getAttribLocation(shader_program, "a_color");
-    // legacygl.a_normal = gl.getAttribLocation(shader_program, "a_normal");
-    // legacygl.a_texcoord = gl.getAttribLocation(shader_program, "a_texcoord");
-    // gl.enableVertexAttribArray(legacygl.a_position);
-    // gl.enableVertexAttribArray(legacygl.a_color);
-    // gl.enableVertexAttribArray(legacygl.a_normal);
-    // gl.enableVertexAttribArray(legacygl.a_texcoord);
     
+    // vertex attributes
     legacygl.vertex_attributes = {};
-    legacygl.add_vertex_attribute = function(attrib_name, attrib_size) {
-        var vertex_attribute = {};
-        vertex_attribute.size = attrib_size;
+    legacygl.add_vertex_attribute = function(name, size) {
+        this.vertex_attributes[name] = {};
+        this.vertex_attributes[name].size = size;
         // array and buffer
-        vertex_attribute.array = [];
-        vertex_attribute.buffer = gl.createBuffer();
+        this.vertex_attributes[name].buffer = gl.createBuffer();
         // current value
-        vertex_attribute.current = [];
-        for (var i = 0; i < attrib_size; ++i)
-            vertex_attribute.current[i] = 0;
+        this.vertex_attributes[name].current = [];
+        for (var i = 0; i < size; ++i)
+            this.vertex_attributes[name].current[i] = 0;
         // shader location
-        vertex_attribute.location = gl.getAttribLocation(shader_program, "a_" + attrib_name);
-        gl.enableVertexAttribArray(vertex_attribute.location);
-        // register
-        this.vertex_attributes[attrib_name] = vertex_attribute;
-        this[attrib_name] = function() {
-            for (var i = 0; i < attrib_size; ++i)
-                this.vertex_attributes[attrib_name].current[i] = arguments[i];
+        this.vertex_attributes[name].location = gl.getAttribLocation(shader_program, "a_" + name);
+        gl.enableVertexAttribArray(this.vertex_attributes[name].location);
+        // current value setter
+        this[name] = function() {
+            for (var i = 0; i < size; ++i)
+                this.vertex_attributes[name].current[i] = arguments[i];
         };
-        
     };
     // special treatment for position attribute
     legacygl.add_vertex_attribute("position", 3);
     delete legacygl.position;
     delete legacygl.vertex_attributes.position.current;
     legacygl.vertex3 = function(x, y, z) {
-        for (var attrib_name in this.vertex_attributes) {
-            var vertex_attribute = this.vertex_attributes[attrib_name];
-            var push_value = attrib_name == "position" ? [x, y, z] : this.vertex_attributes[attrib_name].current;
-            for (var i = 0; i < vertex_attribute.size; ++i)
-                vertex_attribute.array.push(push_value[i]);
+        for (var name in this.vertex_attributes) {
+            var push_value = name == "position" ? [x, y, z] : this.vertex_attributes[name].current;
+            for (var i = 0; i < this.vertex_attributes[name].size; ++i)
+                this.vertex_attributes[name].array.push(push_value[i]);
         }
     };
     legacygl.vertex2 = function(x, y) {
         this.vertex3(x, y, 0);
     };
+    // uniforms
+    legacygl.uniforms = {};
+    legacygl.add_uniform = function(name, type) {
+        var uniform = {};
+
+    };
     // begin and end
     legacygl.begin = function(mode) {
         this.mode = mode;
         // clear array
-        for (var attrib_name in this.vertex_attributes)
-            this.vertex_attributes[attrib_name].array = [];
+        for (var name in this.vertex_attributes)
+            this.vertex_attributes[name].array = [];
     };
     legacygl.end = function() {
-        for (var attrib_name in this.vertex_attributes) {
-            var vertex_attribute = this.vertex_attributes[attrib_name];
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertex_attribute.buffer);
-            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertex_attribute.array), gl.STATIC_DRAW);
+        for (var name in this.vertex_attributes) {
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_attributes[name].buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertex_attributes[name].array), gl.STATIC_DRAW);
         }
-
-        // // position
-        // this.buffer_position = gl.createBuffer();
-        // gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer_position);
-        // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.array_position), gl.STATIC_DRAW);
-        // // color
-        // this.buffer_color = gl.createBuffer();
-        // gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer_color);
-        // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.array_color), gl.STATIC_DRAW);
-        // // normal
-        // this.buffer_normal = gl.createBuffer();
-        // gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer_normal);
-        // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.array_normal), gl.STATIC_DRAW);
-        // // texcoord
-        // this.buffer_texcoord = gl.createBuffer();
-        // gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer_texcoord);
-        // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.array_texcoord), gl.STATIC_DRAW);
     };
+    // draw call with displaylist-like mechanism
     legacygl.draw = function(draw_func) {
         if (!this.is_valid) {
             draw_func();
             this.is_valid = true;
         }
-        for (var attrib_name in this.vertex_attributes) {
-            var vertex_attribute = this.vertex_attributes[attrib_name];
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertex_attribute.buffer);
-            gl.vertexAttribPointer(vertex_attribute.location, vertex_attribute.size, gl.FLOAT, false, 0, 0);
+        for (var name in this.vertex_attributes) {
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_attributes[name].buffer);
+            gl.vertexAttribPointer(this.vertex_attributes[name].location, this.vertex_attributes[name].size, gl.FLOAT, false, 0, 0);
         }
-        // // position
-        // gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer_position);
-        // gl.vertexAttribPointer(this.a_position, 3, gl.FLOAT, false, 0, 0);
-        // // color
-        // gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer_color);
-        // gl.vertexAttribPointer(this.a_color, 4, gl.FLOAT, false, 0, 0);
-        // // normal
-        // gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer_normal);
-        // gl.vertexAttribPointer(this.a_normal, 3, gl.FLOAT, false, 0, 0);
-        // // texcoord
-        // gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer_texcoord);
-        // gl.vertexAttribPointer(this.a_texcoord, 2, gl.FLOAT, false, 0, 0);
-        // draw
         gl.drawArrays(this.mode, 0, this.vertex_attributes.position.array.length / 3);
     };
     legacygl.invalidate = function() {
