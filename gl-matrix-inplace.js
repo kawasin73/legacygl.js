53a58aeb04b7a177648485bbdbee9cb3ce9d127a
Kenshi Takayama <kenshi84@gmail.com>
2015-03-19 18:45:52 +0900

    testing projection matrix

--- gl-matrix-inplace.js
+++ gl-matrix-inplace.js
@@ -10,3 +10,4 @@ mat4.rotateZ_ip = function(a, rad) { mat4.rotateZ(a, a, rad); };
 mat4.scale_ip = function(a, v) { mat4.scale(a, a, v); };
 mat4.translate_ip = function(a, v) { mat4.translate(a, a, v); };
 mat4.transpose_ip = function(a) { mat4.transpose(a, a); };
+mat4.ortho2d = function(a, left, right, bottom, top) { mat4.ortho(a, left, right, bottom, top, -1, 1); };
